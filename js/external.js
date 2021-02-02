ff63f71e240f06549187d7f77634ee5ee01625e5
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-02 11:28:44 -0500

    favorite color input and response added as prompt w/ if/else alert

--- js/external.js
+++ js/external.js
@@ -1,5 +1,11 @@
 "use strict"
 
 alert("Welcome to my Website!");
-
+var favorite_color = prompt("what is the best color in the whole wide world?");
+  if (favorit_color === "blue") {
+    alert(favorite_color + " is the correct answer and is the best color in the whole wide world!!")
+  }
+  else {
+    alert(favorite_color + "this is just plain wrong")
+  };
 console.log("Hello, from external Javascript")
