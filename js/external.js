39503d7ef08841e8d25a4f135d9cea599a4c1e5b
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-02 12:31:44 -0500

    exercise 3.1: movie rental

--- js/external.js
+++ js/external.js
@@ -1,6 +1,8 @@
 "use strict";
-
+// Ensuring user knows they are here!
 alert("Welcome to my Website!");
+
+// does user favorite color match mine!
 var favorite_color = prompt("what is the best color in the whole wide world?");
   if (favorit_color === "blue") {
     alert(favorite_color + " is the correct answer and is the best color in the whole wide world!!")
@@ -8,4 +10,15 @@ var favorite_color = prompt("what is the best color in the whole wide world?");
   else {
     alert(favorite_color + "this is just plain wrong")
   };
+
+//exercise 3.1
+var lil_mermaid = prompt("how many nights did you rent The Little Mermaid?");
+var bro_bear = prompt("how many nights did you rent Brother Bear?");
+var hercules = prompt("how many nights did you rent Hercules");
+var price = prompt("what was the rental rate?");
+
+return alert("You owe $" + (price * (lil_mermaid + bro_bear + hercules)));
+
+
+
 console.log("Hello, from external Javascript");
