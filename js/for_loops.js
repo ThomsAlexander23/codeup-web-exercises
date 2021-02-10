3087d31b2ac40e55b0529c5df40e4471613b84f4
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-10 11:52:47 -0500

    feat: exercise 3 for loop odd/even completed

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,6 +5,19 @@
       console.log(`${number} x ${i} = ${number * i}`);
   }
 
+// exercise 3: for loop odd/even of random numbers
+  for (let i = 0; i < 10; i++)
+    if (i < 10){
+      var x = (Math.floor(Math.random()* 180) + 20);
+      if ((x % 2) === 0){
+        console.log(`${x} is even`);
+      }
+      else{
+        console.log(`${x} is odd`);
+      }
+    }
+
+
 // recursive function for this loop exercise
   // function randomNumber(depth){
   //   if (depth >= 0) {
@@ -19,6 +32,6 @@
   //   }
   // } 
 
-  
+
 
 })();
