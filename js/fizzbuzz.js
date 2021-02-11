2c7f7304dfc444fe4f08920e2bdec18974836c27
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-11 16:02:07 -0500

    Feat: coded the problem and commented code

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,3 +1,23 @@
 (function (){
   "use strict"
+
+  // FizzBuzz Exercise: 
+  // 0-100 if divisible 3 log Fizz
+  // 0-100 if divisible 5 log Buzz
+  //0-100 if divisible 3 & 5 (15) log FizzBuzz
+  for (let i = 0; i <= 100;i++){
+    if (i % 15 === 0){
+      console.log("FizzBuzz");
+      
+    }
+    else if((i % 5) === 0){
+      console.log("Buzz");
+    }
+    else if ((i % 3) === 0){
+      console.log("Fizz");
+    }
+  }
+
+
+
 })
