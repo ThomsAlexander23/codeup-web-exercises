5224309d49b818c43d584b0a5b819773e0d5f42d
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-11 10:40:54 -0500

    Initial commit: created HTML page to run iterating.js an array exercise file

---
+++ js/iterating.js
@@ -0,0 +1,40 @@
+(function(){
+    "use strict";
+
+
+    // Var names created and printed length of names
+    var names = ["Alex", "Rhaegar", "Vasya", "Olesia"];
+    console.log(names.length);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+})();
