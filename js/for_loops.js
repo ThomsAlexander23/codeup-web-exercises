b46604a55e77c5389c9d015847cb3fd9fa6808c7
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-11 18:44:33 -0500

    Feat: removed switch for Exercise 4; added a nested loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -18,60 +18,35 @@
     }
 
 
-// recursive function for this loop exercise
-  // function randomNumber(depth){
-  //   if (depth >= 0) {
-  //     var number = (((Math.floor(Math.random() * (200-20) + 20))));
-  //     if (((number) % 2) === 0){
-  //       console.log(`${number} is even`);
-  //     }
-  //     else {
-  //       console.log(`${number} is odd`);
-  //     }
-  //     randomNumber(depth -1);
-  //   }
-  // } 
-     
-  // exercise 4 half pyramid
-  for (let i = 1; i < 10; i++)
-    switch (i){
-      case 1:
-        console.log(i);
-        break;
-      case 2:
-        console.log(i + "2");
-        break;
-      case 3:
-        console.log(i + "33");
-        break;
-      case 4:
-        console.log(i + "444");
-        break;
-      case 5:
-        console.log(i + "5555");
-        break;
-      case 6:
-        console.log(i + "66666");
-        break;
-      case 7:
-        console.log(i + "777777");
-        break;
-      case 8:
-        console.log(i + "8888888");
-        break;
-      case 9:
-        console.log(i + "99999999");
-        break;
+// Exercise 3: recursive function implemented
+  function randomNumber(depth){
+    if (depth >= 0) {
+      var number = (((Math.floor(Math.random() * (200-20) + 20))));
+      if (((number) % 2) === 0){
+        console.log(`${number} is even`);
+      }
+      else {
+        console.log(`${number} is odd`);
+      }
+      randomNumber(depth -1);
     }
+  } 
 
 // Exercise 4: refactored!!
   var factor = "1";
-  for (let i = 1; i < 10; i++)
-    if (i < 10){
+  for (let i = 1; i < 10; i++){
     var multiplier = Number(factor);
     console.log(i * multiplier);
     factor = (factor + "1");
     }
+//  Exercise 4: nested  
+  for (let i = 1; i < 10; i++){
+    var num =" ";
+    for (let x = 1; x <= i; x++){
+        num += i.toString()
+    }
+    console.log(num);
+  }
 
 // exercise 5: for loop 100 to 5 decrementing by 5
   for (let i = 100; i >= 5; i = i-5)
