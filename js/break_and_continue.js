964c72e97295a4a626352a9d33453c6a7cb460ee
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-12 17:25:14 -0500

    Feat: fixed code to give proper readout and structure as indicated in exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,15 +3,23 @@
 
   // exercise for break and continue
   var numToSkip = Number(prompt("eneter a number between 1-50"));
-  for (let i =0; i <= 50; i++)
-    if ((i % 2) === 0){
-      continue;
+  for (let i = 0; i <= 50; i++){
+    if ((numToSkip > 50) || ((numToSkip % 2)===0)){
+      break
     }
     else {
-      if (i === numToSkip){
-        console.log(`Yikes! Skipping number: ${numToSkip}`);
+      console.log(`The number to skip: ${numToSkip}`);
+      if ((i % 2) === 0){
+        continue;
       }
-      console.log(`Here is an odd number: ${i}`);
+      else {
+        if (i === numToSkip){
+          console.log(`Yikes! Skipping number: ${numToSkip}`);
+        }
+        else{
+          console.log(`Here is an odd number: ${i}`);
+        }
+      }
     }
-
+  } 
 })
