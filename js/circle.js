414e97fff64440fcdb6667ab3119580f1c3ad281
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-12 14:44:08 -0500

    Feat: fixed bug causing display of JS file wrong; completed exercises on circle.js

--- js/circle.js
+++ js/circle.js
@@ -6,19 +6,17 @@
         radius: 3,
 
         getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+             return (Math.PI *(circle.radius ** 2));
         },
 
         logInfo: function (doRounding) {
-            // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true){
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + (Math.round(this.getArea(this.radius))));
+            }
+            else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + (this.getArea(this.radius)));
+            }
+            
         }
     };
 
@@ -29,7 +27,7 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
