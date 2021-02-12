ea9b7652385b8c0cd931466af47ff0651eb4fe7d
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-12 12:09:40 -0500

    Feat: added function to display index+1 title and author on new lines to console; inserted the function to forEach to display all entries in books

--- js/objects.js
+++ js/objects.js
@@ -61,22 +61,18 @@
         {title: "My Ukrainian Marriage", author:{firstName: "Vasya",lastName: "Rhaegar"}}
     ];
 
-    console.log(books[0].title);
-    console.log(books[2].author.firstName);
-    console.log(books[4].author.lastName);
-    /** TODO:
-     * Create an array of objects that represent books and store it in a
-     * variable named `books`. Each object should have a title and an author
-     * property. The author property should be an object with properties
-     * `firstName` and `lastName`. Be creative and add at least 5 books to the
-     * array
-     *
-     * Example:
-     * > console.log(books[0].title) // "The Salmon of Doubt"
-     * > console.log(books[0].author.firstName) // "Douglas"
-     * > console.log(books[0].author.lastName) // "Adams"
-     */
+    // console.log(books[0].title);
+    // console.log(books[2].author.firstName);
+    // console.log(books[4].author.lastName);
 
+    // displayBookInfo is passed books and i to output to console book index+1 title and author to new lines
+    //for each is used to loop through array books with the displayBookInfo function
+    function displayBookInfo(books,i){
+        console.log(`Book # ${i+1}`);
+        console.log(`Title: ${books.title}`);
+        console.log(`Author: ${books.author.firstName} ${books.author.lastName}`);
+    }
+    books.forEach(displayBookInfo);
     /**
      * TODO:
      * Loop through the books array and output the following information about
