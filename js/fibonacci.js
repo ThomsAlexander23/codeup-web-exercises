d0aea153da3ae6b361fcd0d784f7586a0d824507
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-18 18:48:59 -0500

    Feat: corrected error where 2nd fibonacci number was incorrect

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,26 +1,23 @@
-(function (){
-  "use strict"
+(function () {
+  "use strict";
 
-// fibonacci function generated first 50 fibonacci numbers to
-// array logging the array to the console
-  function fibonacciNumbers(depth = prompt("How many fibonacci numbers do you want?")){
-    while ((typeof(depth) !== "number") && (isNaN(depth))){
-      fibonacciNumbers((prompt("A number must be entered!")));
+  // fibonacci function generated first 50 fibonacci numbers to
+  // array logging the array to the console
+  function fibonacciNumbers(
+    depth = prompt("How many fibonacci numbers do you want?")
+  ) {
+    while (typeof depth !== "number" && isNaN(depth)) {
+      fibonacciNumbers(prompt("A number must be entered!"));
     }
     var fibonacciArray = [];
-    for (let i = 0; i < depth; i++){
-      if (i <= 1){
+    for (let i = 0; i < depth; i++) {
+      if (i <= 1) {
         fibonacciArray.push(i);
         console.log(fibonacciArray[i]);
-      } 
-      else if (i === 2) {
-        fibonacciArray.push(i);
+      } else {
+        fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
         console.log(fibonacciArray[i]);
       }
-      else {
-        fibonacciArray.push((fibonacciArray[i-1]) + (fibonacciArray[i-2]));
-        console.log(fibonacciArray[i]);
-      }
-    }  
+    }
   }
-})
+});
