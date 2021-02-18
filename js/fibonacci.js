b937444cdd3d80ce467783d355632ceb45856af6
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-18 17:36:58 -0500

    Feat: refactored if/else to be cleaner and more readable

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,28 +1,24 @@
 (function (){
   "use strict"
-//start at one
-// print first 50 fibonacci numbers
+
+// fibonacci function generated first 50 fibonacci numbers to
+// array logging the array to the console
   function fibonacciNumbers(){
     var fibonacciArray = [];
-    for (let i = 0; i < 50; i++)
-      if (i === 0){
+    for (let i = 0; i < 50; i++){
+      if (i <= 2){
         fibonacci = {}
         fibonacci.iteration = i;
         fibonacci.value = i;
         fibonacciArray.push(fibonacci);
-      }
-      else if (i === 1 || i === 2){
-        fibonacci = {}
-        fibonacci.iteration = i;
-        fibonacci.value = i;
-        fibonacciArray.push(fibonacci);
-      }
+      } 
       else {
         fibonacci = {}
         fibonacci.iteration = i;
         fibonacci.value = ((Number(fibonacciArray[i-1].value)) + (Number(fibonacciArray[i-2].value)));
-        fibonacciArray.push(fibonacci);
-      }
-    console.log(fibonacciArray);
+        fibonacciArray.push(fibonacci)
+    }
   }
+  console.log(fibonacciArray);
+}
 })
