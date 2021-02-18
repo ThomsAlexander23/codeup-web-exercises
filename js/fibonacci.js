6324ae263c1518fd02e05ab9aaf3ab1d76c781bc
ThomsAlexander23 <thoms.alexander23@gmail.com>
2021-02-18 18:42:56 -0500

    Feat: Found bug in previous commit, altered back to proper if/else and added a prompt for how many fibonacci number the user wants

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -3,13 +3,20 @@
 
 // fibonacci function generated first 50 fibonacci numbers to
 // array logging the array to the console
-  function fibonacciNumbers(){
+  function fibonacciNumbers(depth = prompt("How many fibonacci numbers do you want?")){
+    while ((typeof(depth) !== "number") && (isNaN(depth))){
+      fibonacciNumbers((prompt("A number must be entered!")));
+    }
     var fibonacciArray = [];
-    for (let i = 0; i < 50; i++){
-      if (i <= 2){
+    for (let i = 0; i < depth; i++){
+      if (i <= 1){
         fibonacciArray.push(i);
         console.log(fibonacciArray[i]);
       } 
+      else if (i === 2) {
+        fibonacciArray.push(i);
+        console.log(fibonacciArray[i]);
+      }
       else {
         fibonacciArray.push((fibonacciArray[i-1]) + (fibonacciArray[i-2]));
         console.log(fibonacciArray[i]);
