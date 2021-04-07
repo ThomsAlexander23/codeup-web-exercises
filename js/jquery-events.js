2c2938559b4b30bc7c5f030ffc9b753e28df8c53
Alex <thoms.alexander23@gmail.com>
2021-04-07 12:09:09 -0400

    Feat: continued working on input field bugs

--- js/jquery-events.js
+++ js/jquery-events.js
@@ -1,42 +1,53 @@
-(function () {
-    $(document).ready(function () {
+// (function () {
+
+    // $(document).ready(function () {
         /*
 
         ----------- koonami ----------
 
         */
+        // ------ vars for webpage ----- //
+//         var codeEntered = false;
+//         var koonamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+//         var keystrokes = [];
+//
+//         // ----- function for koonami ---- //
+//         function loopFunction(array) {
+//             var count = 0;
+//             for (let i = 0; i < koonamiCode.length; i++) {
+//                 if (count < 10) {
+//                     if (koonamiCode[i] !== array[i]) {
+//                         count = 0;
+//                         return false;
+//                     } else if (koonamiCode[i] === array[i]) {
+//                         count += 1;
+//                     }
+//                 }
+//             }
+//             if (count === 10) {
+//                 return true;
+//             }
+//             return false;
+//         }
+//
+//         //---- koonami check ---- //
+// if (codeEntered === false){
+//     $(document).keydown(function(event) {
+//         if (keystrokes.length !== 10) {
+//             keystrokes.push(event.key);
+//             console.log(keystrokes)
+//         } else if (keystrokes.length === 10 && loopFunction(keystrokes) === true) {
+//             if (event.key === "Enter") {
+//                 codeEntered = true;
+//             }
+//         }
+//     });
+// }
+// else if (codeEntered === true){
+//     console.log('fuck you')
+// }
 
-        var koonamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
-        var keystrokes = [];
-        $('#search').keydown(function(event) {
-            if (keystrokes.length !== 10) {
-                keystrokes.push(event.key);
-                console.log(keystrokes)
-            } else if (keystrokes.length === 10 && loopFunction(keystrokes) === true) {
-                if (event.key === "Enter") {
-                    event.preventDefault()
-                    $('body').css('background-color', 'black')
-                }
-            }
-        });
 
-        function loopFunction(array) {
-            var count = 0;
-            for (let i = 0; i < koonamiCode.length; i++) {
-                if (count < 10) {
-                    if (koonamiCode[i] !== array[i]) {
-                        count = 0;
-                        return false;
-                    } else if (koonamiCode[i] === array[i]) {
-                        count += 1;
-                    }
-                }
-            }
-            if (count === 10) {
-                return true;
-            }
-            return false;
-        }
 
 
         /*
@@ -54,5 +65,5 @@
         // $('.form li').mouseleave(function(){
         //     $('.form li').css('color' , 'black')
         // });
-    });
-})();
+    // });
+// })();
